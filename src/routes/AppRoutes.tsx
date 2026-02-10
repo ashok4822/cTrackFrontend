ec3b0949b8d6e3fca06a8a36c862eea45ce90515
arun <arun@gmail.com>
2026-02-10 15:38:09 +0530

    Not Found page added

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //Pages
 import LandingPage from "@/pages/LandingPage";
+import NotFound from "@/pages/NotFound";
 
 const AppRoutes = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          {/* Landing/Home page */}
           <Route path="/" element={<LandingPage />} />
+
+          {/* Catch-all*/}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
